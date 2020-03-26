d6e3f7446d612db421817c6cf0248dbf3ec720bd
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-26 07:27:42 +0200

    add model Client Entity

--- src/main/java/com/psy888/thymeleaftest/model/ClientEntity.java
+++ src/main/java/com/psy888/thymeleaftest/model/ClientEntity.java
@@ -1,7 +1,24 @@
 package com.psy888.thymeleaftest.model;
 
-import javax.persistence.Entity;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
 public class ClientEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message="Clients's name is required")
+    private String name;
+
+    @NotBlank(message="Client's phone is required")
+    private String phone;
 }
